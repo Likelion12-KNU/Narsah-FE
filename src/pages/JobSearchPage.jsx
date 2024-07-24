f47f235d12ae8efd29e60bbf7be0fc29655b3c92
a.chae <dkscodus1577@naver.com>
2024-07-24 23:08:53 +0900

    import img

--- src/pages/JobSearchPage.jsx
+++ src/pages/JobSearchPage.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Bar from '../components/Bar';
 import "../style/JobSearchPage.css"
+import userOrange from '../img/user_orange.png';
+import buildingOrange from '../img/building_orange.png';
 
 function JobSearchPage() {
 
