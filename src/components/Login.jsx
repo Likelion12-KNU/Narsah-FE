63cd22ee651728e53e52ae5c575fd9c36439a1bf
rjsdn031 <rjsdn031@naver.com>
2024-07-26 03:25:33 +0900

    Feat: 로그인 화면 컴포넌트 분리 및 더미 데이터 사용 임시 구현
    LoginPage의 로그인 파트 <Login />으로 분리.
    tmpUser 값 사용하여 임시 로그인 기능 -> id, pw 확인만

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+// dummy
+import { tUser } from '../config/tmpUser'
+
+const Login = () => {
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+
+    // 더미 데이터(tUser) 사용. BE와 연동시 수정
+    const handleConfirm = (e) => {
+        if (id === tUser.id && password === tUser.password) {
+            alert("로그인에 성공했습니다.");
+            // user profile 또는 main으로 이동
+        } else {
+            alert("ID 또는 PASSWORD가 잘못되었습니다.")
+        }
+    }
+
+    return (
+        <div className='content'>
+            <form className='loginForm'>
+                <h1>LOG IN</h1>
+                <input 
+                    type='text'
+                    placeholder='ID'
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                />
+                <input
+                    type='password'
+                    placeholder='PASSWORD'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button 
+                    type='submit'
+                    onClick={handleConfirm}
+                >LOG IN</button>
+                <br/><p>계정이 없으신가요? </p><Link to='/signup'>회원가입</Link>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
