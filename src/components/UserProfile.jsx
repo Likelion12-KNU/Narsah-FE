b14ec1364e26b800a6ff18016dc7a3b39727f4f4
rjsdn031 <rjsdn031@naver.com>
2024-08-02 18:27:16 +0900

    Fix: UserProfile login server 연동
    UserProfile에서 fetch 받아서 로그인 유무 확인
    useState 사용 (isLoggedin, user)
    로그인 안 됐을 시 로그인해주세요 출력
    
    빠른 배포 후 테스트 요망

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,22 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import profileImg from "../img/profile.png";
 import userOrange from '../img/user_orange.png';
+import { baseUrl } from '../config/const';
 import "../style/UserProfile.css"
 
 // code : 일반 유저 -> 0, 간병인 -> 1
 function UserProfile({name, userid, code}) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 관리하는 상태 변수
+    const [user, setUser] = useState("");
+
+    useEffect(() => {
+        fetch(`${baseUrl}/api/auth/check-session`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" }
+            }).then(response => {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json();
+                } else {
+                    throw new Error('Not logged in');
+                }
+            }).then(jsonData => {
+                setUser(jsonData.email);
+                setIsLoggedIn(true);
+                console.log(jsonData);
+            }).catch(error => {
+                console.error("Fetch error: ", error);
+            });
+    }, []);
+
+    if (!isLoggedIn) {
+        return <div>로그인해주세요</div>;
+    }
 
     return (
         <div className="profile">
             <div className="userHeader">
                 <img src={userOrange} alt="user" className="userIcon" />
                 <div className="userInfo">
-                    {code === 0 ? (
-                        <h2><strong>{name}</strong> 님</h2>
-                    ) : (
-                        <h2>간병사 <strong>{name}</strong> 님</h2>
-                    )}
+                    {
+                        code === 0 ? (
+                            <h2><strong>{name}</strong> 님</h2>
+                        ) : (
+                            <h2>간병사 <strong>{name}</strong> 님</h2>
+                        )
+                    }
                     
                     <p>{userid}</p>
                 </div>
