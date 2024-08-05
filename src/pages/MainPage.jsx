20ff0bbca677403065822c692193833a1452f709
a.chae <dkscodus1577@naver.com>
2024-08-05 19:51:01 +0900

    border none

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -85,7 +85,7 @@ function MainPage() {
                 <div className='nav'>
                     {isLoggedIn ? (
                         <>
-                            <button className='navElement.logoutButton' onClick={handleLogout}>
+                            <button className='navElement' onClick={handleLogout}>
                                 LOGOUT
                             </button>
                             <Link className='navElement' to="/user">
